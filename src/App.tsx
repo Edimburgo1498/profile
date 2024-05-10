ec5716b31c03af70e6ee26a883c028d9e6d05692
Edimburgo1498 <carlos.anzaldo92@gmail.com>
2024-05-10 16:03:54 +0200

    new commit

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,11 @@ function App() {
             </h4>
             <div className="icons">
               <p>
-                <i className="fa fa-envelope-o" id="mail" aria-hidden="true"></i>
+                <i
+                  className="fa fa-envelope-o"
+                  id="mail"
+                  aria-hidden="true"
+                ></i>
               </p>
               <p>
                 <i className="fa fa-phone" id="phone" aria-hidden="true"></i>
@@ -35,15 +39,16 @@ function App() {
                 <i className="fa fa-github" id="github" aria-hidden="true"></i>
               </p>
               <p>
-                <i className="fa fa-linkedin-square" id="square" aria-hidden="true"></i>
+                <i
+                  className="fa fa-linkedin-square"
+                  id="square"
+                  aria-hidden="true"
+                ></i>
               </p>
             </div>
           </div>
           <div className="facebook" id="facebook" ref={elemet}>
-            <img
-              src="../../public/perfil.jpg"
-              className="img"
-            />
+            <img src="../../public/perfil.jpg" className="img" />
           </div>
         </section>
         <section className="article-2">
@@ -71,16 +76,17 @@ function App() {
             }}
           >
             <h5>Lightia - full stack developer</h5>
-            Colabore en la creación de aplicaciones web utilizando el stack
-            MERN, me encargaba del mantenimiento de la aplicación y él
-            desarrollo de nuevas funcionalidades para distintos sistemas,
-            responsable del mantenimiento de las bases de datos y las
-            constantes actualizaciones que se requerían,  seguridad de lo
-            Sistemas y lógica de negocio.
-            <h5 style={{marginTop: '3%'}}>Cti - Soporte TI soporte</h5>
-            + creación de reportes de errores y fallos, implementación de correcciones.
-            + capacidad para solucionar problemas operativos vía remota.
-            + mantenimiento de sistemas para garantizar el correcto funcionamiento.
+            Colabore en la creación de aplicaciones web utilizando el stack    
+                    MERN, me encargaba del mantenimiento de la aplicación y él  
+                      desarrollo de nuevas funcionalidades para distintos
+            sistemas,             responsable del mantenimiento de las bases de
+            datos y las             constantes actualizaciones que se requerían,
+            seguridad de lo Sistemas y lógica de negocio.
+            <h5 style={{ marginTop: "3%" }}>Cti - Soporte TI soporte</h5>+
+            creación de reportes de errores y fallos, implementación de
+            correcciones. + capacidad para solucionar problemas operativos vía
+            remota. + mantenimiento de sistemas para garantizar el correcto
+            funcionamiento.
           </p>
         </section>
         <section className="article-4">
@@ -99,18 +105,22 @@ function App() {
         </section>
         <section className="article-5">
           <h3>project</h3>
-          <div className="grid-container" >
-            <div style={{ height: "15vh", width: "180px" }}>
-              <img src='' alt="" className="picture" />
+          <div className="grid-container">
+            <div className="project">
+              <a href="https://tetris-dy16.vercel.app/" className="link">
+                <img src="../../Tetris.png" alt="" className="picture" />
+                <div className="description">
+                  <p className="descriptio">Testris</p>
+                </div>
+              </a>
+            </div>
+            <div className="project">
+              <img src="" alt="" className="picture" />
               <div className="description"></div>
             </div>
-            <div style={{ height: "15vh", width: "180px" }}>
-            <img src="" alt="" className="picture" />
-            <div className="description"></div>
-            </div>
-            <div style={{ height: "15vh", width: "180px"}}>
-            <img src="" alt="" className="picture" />
-            <div className="description"></div>
+            <div className="project">
+              <img src="" alt="" className="picture" />
+              <div className="description"></div>
             </div>
           </div>
         </section>
@@ -140,18 +150,18 @@ function App() {
               alt="bootstrap"
               width="50"
               height="50"
-            /> 
+            />
             <a
               href="https://www.w3schools.com/css/"
               target="_blank"
               rel="noreferrer"
-            /> 
+            />
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/css3/css3-original-wordmark.svg"
               alt="css3"
               width="50"
               height="50"
-            /> 
+            />
             <a
               href="https://www.docker.com/"
               target="_blank"
@@ -162,58 +172,58 @@ function App() {
               alt="docker"
               width="50"
               height="50"
-            /> 
+            />
             <a href="https://expressjs.com" target="_blank" rel="noreferrer" />
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/express/express-original-wordmark.svg"
               alt="express"
               width="50"
               height="50"
-            /> 
-            <a href="https://www.figma.com/" target="_blank" rel="noreferrer" /> 
+            />
+            <a href="https://www.figma.com/" target="_blank" rel="noreferrer" />
             <img
               src="https://www.vectorlogo.zone/logos/figma/figma-icon.svg"
               alt="figma"
               width="50"
               height="50"
-            /> 
-            <a href="https://git-scm.com/" target="_blank" rel="noreferrer" /> 
+            />
+            <a href="https://git-scm.com/" target="_blank" rel="noreferrer" />
             <img
               src="https://www.vectorlogo.zone/logos/git-scm/git-scm-icon.svg"
               alt="git"
               width="50"
               height="50"
-            /> 
+            />
             <a
               href="https://www.w3.org/html/"
               target="_blank"
               rel="noreferrer"
-            /> 
+            />
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/html5/html5-original-wordmark.svg"
               alt="html5"
               width="50"
               height="50"
-            /> 
+            />
             <a
               href="https://developer.mozilla.org/en-US/docs/Web/JavaScript"
               target="_blank"
               rel="noreferrer"
-            /> 
+            />
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg"
               alt="javascript"
               width="50"
               height="50"
-            /> 
-            <a href="https://www.linux.org/" target="_blank" rel="noreferrer" /> 
+            />
+            <a href="https://www.linux.org/" target="_blank" rel="noreferrer" />
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg"
               alt="linux"
               width="50"
               height="50"
-            /> 
-            <a href="https://mariadb.org/" target="_blank" rel="noreferrer" /> 
+            />
+            <a href="https://mariadb.org/" target="_blank" rel="noreferrer" />
             <img
               src="https://www.vectorlogo.zone/logos/mariadb/mariadb-icon.svg"
               alt="mariadb"
@@ -224,55 +234,55 @@ function App() {
               href="https://www.mongodb.com/"
               target="_blank"
               rel="noreferrer"
-            /> 
+            />
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg"
               alt="mongodb"
               width="50"
               height="50"
-            /> 
-            <a href="https://www.mysql.com/" target="_blank" rel="noreferrer" /> 
+            />
+            <a href="https://www.mysql.com/" target="_blank" rel="noreferrer" />
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg"
               alt="mysql"
               width="50"
               height="50"
-            /> 
+            />
             <a href="https://www.nginx.com" target="_blank" rel="noreferrer" />
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nginx/nginx-original.svg"
               alt="nginx"
               width="50"
               height="50"
-            /> 
+            />
             <a href="https://nodejs.org" target="_blank" rel="noreferrer" />
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg"
               alt="nodejs"
               width="50"
               height="50"
-            /> 
+            />
             <a href="https://postman.com" target="_blank" rel="noreferrer" />
             <img
               src="https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg"
               alt="postman"
               width="50"
               height="50"
-            /> 
-            <a href="https://reactjs.org/" target="_blank" rel="noreferrer" /> 
+            />
+            <a href="https://reactjs.org/" target="_blank" rel="noreferrer" />
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg"
               alt="react"
               width="50"
               height="50"
-            /> 
+            />
             <a href="https://redux.js.org" target="_blank" rel="noreferrer" />
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/master/icons/redux/redux-original.svg"
               alt="redux"
               width="50"
               height="50"
-            /> 
+            />
             <a
               href="https://www.typescriptlang.org/"
               target="_blank"
@@ -283,8 +293,8 @@ function App() {
               alt="typescript"
               width="50"
               height="50"
-            /> 
-            <a href="https://webpack.js.org" target="_blank" rel="noreferrer" /> 
+            />
+            <a href="https://webpack.js.org" target="_blank" rel="noreferrer" />
             <img
               src="https://raw.githubusercontent.com/devicons/devicon/d00d0969292a6569d45b06d3f350f463a0107b0d/icons/webpack/webpack-original-wordmark.svg"
               alt="webpack"
